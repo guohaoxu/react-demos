c57c42365021784352121c34c2c0aa72ac253544
guohao <guohao0817@gmail.com>
2016-07-05 11:10:08 +0800

    update

---
+++ flux-todo/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: [
+        './public/src/entry.js'
+    ],
+    output: {
+        path: __dirname + '/public/build',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    }
+}
