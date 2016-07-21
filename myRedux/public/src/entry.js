55dfe459b641fba54f2ceb8aeefd9fc17397987e
guohao <guohao0817@gmail.com>
2016-07-21 17:49:38 +0800

    update

--- myRedux/public/src/entry.js
+++ myRedux/public/src/entry.js
@@ -1,32 +1,8 @@
-/**
-* actions
-*/
-export const ADD_TODO = 'ADD_TODO';
-export const TOGGLE_TODO = 'TOGGLE_TODO';
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-/**
-* other consts
-*/
-export const VisibilityFilters = {
-	SHOW_ALL: 'SHOW_ALL',
-	SHOW_COMPLETED: 'SHOW_COMPLETED',
-	SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
-
-/**
-* action function
-*/
-export function addTodo(text) {
-	return { type: ADD_TODO, text }
-}
-export function toggleTodo(index) {
-	return { type: TOGGLE_TODO, index}
-}
-export function setVisibilityFilter(filter) {
-	return { type: SET_VISIBILITY_FILTER, filter}
-}
-
-/**
-* reducer
-*/
+ReactDOM.render(
+    <App />,
+    document.getElementById('content')
+);
