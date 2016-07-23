3900c720736dca363609904f7844c695900ae8a5
guohao <guohao0817@gmail.com>
2016-07-24 03:44:54 +0800

    update

--- myRedux/public/KanbanBoard.js
+++ myRedux/public/KanbanBoard.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+mport React, { Component, PropTypes } from 'react'
 import { render } from 'react-dom'
 import update from 'react-addons-update'
 import 'whatwg-fetch'
