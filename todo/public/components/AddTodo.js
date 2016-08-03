35394570800d03836237b4c73d895767cce37bea
guohao <guohao0817@gmail.com>
2016-08-03 12:09:54 +0800

    update

--- todo/public/components/AddTodo.js
+++ todo/public/components/AddTodo.js
@@ -1,13 +1,5 @@
 import React , { findDOMNode, Component, PropTypes } from 'react'
 export default class AddTodo extends Component {
-  render() {
-    return (
-      <header className="header">
-				<h1>todos</h1>
-				<input className="new-todo" placeholder="What needs to be done?" ref="input" onKeyPress={e => this.handlePress(e)} />
-			</header>
-    )
-  }
   handlePress(e) {
     if (e.key === 'Enter') {
       const node = this.refs.input
@@ -16,6 +8,14 @@ export default class AddTodo extends Component {
       this.props.onAddPress(text)
       node.value = ''
     }
+  }
+  render() {
+    return (
+      <header className="header">
+				<h1>todos</h1>
+				<input className="new-todo" placeholder="What needs to be done?" ref="input" onKeyPress={e => this.handlePress(e)} />
+			</header>
+    )
   }
 }
 AddTodo.propTypes = {
