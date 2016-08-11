8e16f172d4e6294c76cd0ad4c9a3b8287b822a86
guohao <guohao0817@gmail.com>
2016-08-11 17:55:39 +0800

    update

--- blog/server/routes/index.js
+++ blog/server/routes/index.js
@@ -278,6 +278,7 @@ module.exports = function (app) {
       })
     })
   })
+  
   app.post('/api/post', checkLogin, function (req, res) {
     var title = req.body.title,
       tags = req.body.tags,
@@ -333,7 +334,8 @@ module.exports = function (app) {
         username: req.body.comment.username || '',
         website: req.body.comment.website || '',
         content: req.body.comment.content || '',
-        time: new Date()
+        time: new Date(),
+        id: Date.now()
       }
     Article.findOneAndUpdate({
       _id: _id
@@ -363,158 +365,5 @@ module.exports = function (app) {
       ctx: process.env.staticDomain ? process.env.staticDomain : 'http://localhost:' + app.get('port')
     })
   })
-
-  //get cards
-  app.get('/cards', function (req, res, next) {
-    Card.find({}).sort({id: 1}).exec(function (err, r) {
-      if (err) return next(err)
-      res.send(r)
-    })
-  })
-
-  // new card
-  app.post('/cards', (req, res, next) => {
-    let card = req.body,
-      newCard = new Card(card)
-    newCard.save(function(err, r) {
-      if (err) return next(err)
-      res.json({
-        success: true
-      })
-    })
-  })
-
-  // new card task
-  app.post('/cards/:cardId/tasks', (req, res) => {
-    let cardId = Number(req.params.cardId),
-      newTask = req.body
-    Card.findOneAndUpdate({
-      id: cardId
-    }, {
-      $push: {
-        tasks: newTask
-      }
-    }, function (err, r) {
-      if (err) return console.error(err)
-      res.json({
-        success: true
-      })
-    })
-  })
-
-  // delete card task
-  app.delete('/cards/:cardId/tasks/:taskId', (req, res) => {
-    let cardId = Number(req.params.cardId),
-      taskId = Number(req.params.taskId)
-    Card.findOneAndUpdate({
-      id: cardId
-    }, {
-      $pull: {
-        tasks: {
-          id: taskId
-        }
-      }
-    }, function (err, r) {
-      if (err) return console.error(err)
-      res.json({
-        success: true
-      })
-    })
-  })
-
-  // toggle card task
-  app.put('/cards/:cardId/tasks/:taskId', (req, res) => {
-    let cardId = Number(req.params.cardId),
-      taskId = Number(req.params.taskId),
-      newDoneValue = req.body.done
-    Card.findOneAndUpdate({
-      id: cardId,
-      'tasks.id': taskId
-    }, {
-      $set: {
-        'tasks.$.done': newDoneValue
-      }
-    }, function (err, r) {
-      if (err) return console.error(err)
-      res.json({
-        success: true
-      })
-    })
-  })
-
-  // put card -> two ways: put status and edit card
-  app.put('/cards/:cardId', (req, res) => {
-    let put = req.body.do
-    let cardId = Number(req.params.cardId)
-    if (put === 'updateStatus') {
-      let newStatus = req.body.newStatus
-      Card.findOneAndUpdate({
-        id: cardId
-      }, {
-        $set: {
-          status: newStatus
-        }
-      }, function (err, r) {
-        if (err) return console.error(err)
-        res.json({
-          success: true
-        })
-      })
-    } else if (put === 'edit') {
-      let newCard = req.body.newCard
-      Card.findOneAndUpdate({
-        id: cardId
-      }, {
-        $set: {
-          title: newCard.title,
-          description: newCard.description,
-          color: newCard.color,
-          status: newCard.status
-        }
-      }, function (err, r) {
-        if (err) return console.error(err)
-        res.json({
-          success: true
-        })
-      })
-    }
-  })
-
-  // update card sort
-  app.post('/cards/updatePosition', (req, res) => {
-    let cardId = Number(req.body.cardId)
-    let afterId = Number(req.body.afterId)
-    Card.findOneAndUpdate({
-      id: cardId
-    }, {
-      $set: {
-        id: afterId + 1
-      }
-    } ,function (err, r) {
-      if (err) return console.error(err)
-      Card.findOneAndUpdate({
-        id: afterId
-      }, {
-        $set: {
-          id: cardId
-        }
-      }, function (err, r) {
-        if (err) return console.error(err)
-        Card.findOneAndUpdate({
-          id: afterId + 1
-        }, {
-          $set: {
-            id: afterId
-          }
-        }, function (err, r) {
-          if (err) return console.error(err)
-          res.json({
-            success: true
-          })
-        })
-      })
-    })
-  })
-
 
 }
