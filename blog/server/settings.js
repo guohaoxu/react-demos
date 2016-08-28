f18e708d808773ad67be904ddec2a108ddb75257
guohao <guohao0817@gmail.com>
2016-08-28 23:42:04 +0800

    update

--- blog/server/settings.js
+++ blog/server/settings.js
@@ -1,4 +1,5 @@
 module.exports = {
   cookieSecret: 'guohao-cookie-secret',
-  dbURL: 'mongodb://localhost:27017/guohao-react'
+  // dbURL: 'mongodb://localhost:27017/guohao-blog'
+  dbURL: 'mongodb://guohaoxu:guohao2016\@mlab@ds015869.mlab.com:15869/guohao-blog'
 }
